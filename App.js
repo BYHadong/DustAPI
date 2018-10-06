9488fa0669344982282ffb90e08af65b116b483b
jungpaeng <meis1541@naver.com>
2018-10-06 15:22:35 +0900

    코드 포맷 변경
     - 라인 개행

--- App.js
+++ App.js
@@ -67,15 +67,17 @@ class App extends Component {
   http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?ServiceKey=%2BBmtkr7EQF%2B1UeRAHdORtzXF%2BNVqw%2B2vZR4RdlIRKVXybmj9CU6NKdzJXthecSIwYxyMF2MJWWpMGkTQS8MrLA%3D%3D&_returnType=json&sidoName=%EC%84%9C%EC%9A%B8
   */
   render() {
+    const {mise} = this.state;
+
     let title = this.state.mise.map((item, i) => item.stationName)
     let data = this.state.mise.map((item, i) => item.pm10Value)
     return (
       <View>
         <Picker
           selectedValue={this.state.sidoName}
-          onValueChange={(itemValue, itemIndex) => this.setState({ sidoName: itemValue })}
+          onValueChange={(itemValue) => this.setState({ sidoName: itemValue })}
         >
-          <Picker.Item label="서울" value="서울" />
+          <Picker.Item label="서울" value="서울" />   
           <Picker.Item label="경기" value="경기" />
           <Picker.Itzm label="인천" value="인천" />
         </Picker>
